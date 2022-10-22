fe82b0b2e885a670206ec7628ab0584f5bbe59f1
Matias Arce <matiasarce214@gmail.com>
2022-10-22 20:58:51 -0300

    Edicion

---
+++ js/script.js
@@ -0,0 +1,32 @@
+const usuarios = [{
+    nombre: 'Matías',
+    correo: 'matias@gmail.com',
+    clave: '123'
+},{
+    nombre: 'Daniel',
+    correo: 'daniel@gmail.com',
+    clave: '456'
+},{
+    nombre: 'León',
+    correo: 'leon@gmail.com',
+    clave: '789'
+}]
+
+const juegos = [{
+    nombre: 'Call of Duty Modern Warfare 2',
+    genero: 'Guerra',
+    consola: 'Playstation 5',
+    precio: 30000
+},{
+    nombre: 'Fifa 23',
+    genero: 'Deportes',
+    consola: 'Xbox Series X',
+    precio: 25000
+},{
+    nombre: 'Age of Empires IV',
+    genero: 'Estrategia',
+    consola: 'PC',
+    precio: 12500
+}]
+
+const btnLogin = document.getElementById('boton');
